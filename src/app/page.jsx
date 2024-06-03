6f177acb8523592425eff1a3831399f9b0c3541e
mubashar <intrestingcodding@gmail.com>
2024-06-03 04:14:52 -0700

    change hero section style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,9 @@ const page = () => {
   return (
     <div
       style={{ backgroundPosition: "bottom center" }}
-      className=" min-h-[90vh] bg-no-repeat bg-[url(https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/hero-bg.png)] bg-[#f7f8fd] py-16  flex  max-w-6xl mx-auto px-4"
+      className=" min-h-[90vh] bg-no-repeat bg-[url(https://demo.awaikenthemes.com/weebix/wp-content/uploads/2024/04/hero-bg.png)] bg-[#f7f8fd] py-16  flex  flex-wrap max-w-6xl mx-auto px-4"
     >
-      <div className="py-10 max-lg:w-2/3 w-full">
+      <div className="py-10 w-full">
         <div className="">
           <h3 className="text-[#262f6e] py-2 text-lg">About Edify Elite</h3>
           <h1 className="text-6xl font-semibold mb-2 text-black-800">
@@ -27,8 +27,8 @@ const page = () => {
         </div>
       </div>
 
-      <div className="max-lg:w-1/3 md:w-full py-10 flex justify-center ">
-        <div className=" ">
+      <div className=" w-full py-10 flex justify-center ">
+        <div className="w-full ">
           <iframe
             className="w-full"
             width="560"
