e75d50434de2ec3341c2d693f9ca4aab1473be79
mubashar <intrestingcodding@gmail.com>
2024-06-13 00:27:51 -0700

    error resolved

--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -42,11 +42,11 @@ const Slider2 = () => {
                             <Star size={20} className='text-yellow-400 ' />
                         </div>
                         <p className='line-clamp-4 text-lg  text-gray-400'>
-                            The Common App felt like a labyrinth, and applying to  Yale seemed like a distant dream. Edify Elite became my guiding light! Their team,   particularly their understanding of Pakistani cultural nuances, helped me confidently navigate the application. They didn't just focus on forms; they helped me craft a compelling narrative that resonated with admissions officers. I'm convinced Edify Elite played a major role in my acceptance to Yale!
+                            The Common App felt like a labyrinth and applying to  Yale seemed like a distant dream Edify Elite became my guiding light Their team   particularly their understanding of Pakistani cultural nuances helped me confidently navigate the application They didnt just focus on forms they helped me craft a compelling narrative that resonated with admissions officers Im convinced Edify Elite played a major role in my acceptance to Yale
 
                             Amina Khan
                             Yale University
-                            Ivy League dreams were always on my mind, but standing out in the competitive pool felt impossible. Partnering with Edify Elite was a game-changer. Their expert team provided valuable insights on impressing admissions committees at top universities. They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively. Thanks to Edify Elite, I'm celebrating my acceptance to Cornell University! I highly recommend them to any student aiming for an Ivy League education.
+                            Ivy League dreams were always on my mind but standing out in the competitive pool felt impossible Partnering with Edify Elite was a game changer Their expert team provided valuable insights on impressing admissions committees at top universities They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively Thanks to Edify Elite Im celebrating my acceptance to Cornell University I highly recommend them to any student aiming for an Ivy League education
 
                             Bilal Ahmed
                             Cornell University
@@ -74,11 +74,11 @@ const Slider2 = () => {
                             <Star size={20} className='text-yellow-400 ' />
                         </div>
                         <p className='line-clamp-4 text-lg  text-gray-400'>
-                            The Common App felt like a labyrinth, and applying to  Yale seemed like a distant dream. Edify Elite became my guiding light! Their team,   particularly their understanding of Pakistani cultural nuances, helped me confidently navigate the application. They didn't just focus on forms; they helped me craft a compelling narrative that resonated with admissions officers. I'm convinced Edify Elite played a major role in my acceptance to Yale!
+                            The Common App felt like a labyrinth and applying to  Yale seemed like a distant dream Edify Elite became my guiding light Their team   particularly their understanding of Pakistani cultural nuances helped me confidently navigate the application They didnt just focus on forms they helped me craft a compelling narrative that resonated with admissions officers Im convinced Edify Elite played a major role in my acceptance to Yale
 
                             Amina Khan
                             Yale University
-                            Ivy League dreams were always on my mind, but standing out in the competitive pool felt impossible. Partnering with Edify Elite was a game-changer. Their expert team provided valuable insights on impressing admissions committees at top universities. They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively. Thanks to Edify Elite, I'm celebrating my acceptance to Cornell University! I highly recommend them to any student aiming for an Ivy League education.
+                            Ivy League dreams were always on my mind but standing out in the competitive pool felt impossible Partnering with Edify Elite was a game changer Their expert team provided valuable insights on impressing admissions committees at top universities They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively Thanks to Edify Elite Im celebrating my acceptance to Cornell University I highly recommend them to any student aiming for an Ivy League education
 
                             Bilal Ahmed
                             Cornell University
@@ -106,11 +106,11 @@ const Slider2 = () => {
                             <Star size={20} className='text-yellow-400 ' />
                         </div>
                         <p className='line-clamp-4 text-lg  text-gray-400'>
-                            The Common App felt like a labyrinth, and applying to  Yale seemed like a distant dream. Edify Elite became my guiding light! Their team,   particularly their understanding of Pakistani cultural nuances, helped me confidently navigate the application. They didn't just focus on forms; they helped me craft a compelling narrative that resonated with admissions officers. I'm convinced Edify Elite played a major role in my acceptance to Yale!
+                            The Common App felt like a labyrinth and applying to  Yale seemed like a distant dream Edify Elite became my guiding light Their team   particularly their understanding of Pakistani cultural nuances helped me confidently navigate the application They didnt just focus on forms they helped me craft a compelling narrative that resonated with admissions officers Im convinced Edify Elite played a major role in my acceptance to Yale
 
                             Amina Khan
                             Yale University
-                            Ivy League dreams were always on my mind, but standing out in the competitive pool felt impossible. Partnering with Edify Elite was a game-changer. Their expert team provided valuable insights on impressing admissions committees at top universities. They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively. Thanks to Edify Elite, I'm celebrating my acceptance to Cornell University! I highly recommend them to any student aiming for an Ivy League education.
+                            Ivy League dreams were always on my mind but standing out in the competitive pool felt impossible Partnering with Edify Elite was a game changer Their expert team provided valuable insights on impressing admissions committees at top universities They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively Thanks to Edify Elite Im celebrating my acceptance to Cornell University I highly recommend them to any student aiming for an Ivy League education
 
                             Bilal Ahmed
                             Cornell University
@@ -138,11 +138,11 @@ const Slider2 = () => {
                             <Star size={20} className='text-yellow-400 ' />
                         </div>
                         <p className='line-clamp-4 text-lg  text-gray-400'>
-                            The Common App felt like a labyrinth, and applying to  Yale seemed like a distant dream. Edify Elite became my guiding light! Their team,   particularly their understanding of Pakistani cultural nuances, helped me confidently navigate the application. They didn't just focus on forms; they helped me craft a compelling narrative that resonated with admissions officers. I'm convinced Edify Elite played a major role in my acceptance to Yale!
+                            The Common App felt like a labyrinth and applying to  Yale seemed like a distant dream Edify Elite became my guiding light Their team   particularly their understanding of Pakistani cultural nuances helped me confidently navigate the application They didnt just focus on forms they helped me craft a compelling narrative that resonated with admissions officers Im convinced Edify Elite played a major role in my acceptance to Yale
 
                             Amina Khan
                             Yale University
-                            Ivy League dreams were always on my mind, but standing out in the competitive pool felt impossible. Partnering with Edify Elite was a game-changer. Their expert team provided valuable insights on impressing admissions committees at top universities. They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively. Thanks to Edify Elite, I'm celebrating my acceptance to Cornell University! I highly recommend them to any student aiming for an Ivy League education.
+                            Ivy League dreams were always on my mind but standing out in the competitive pool felt impossible Partnering with Edify Elite was a game changer Their expert team provided valuable insights on impressing admissions committees at top universities They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively Thanks to Edify Elite Im celebrating my acceptance to Cornell University I highly recommend them to any student aiming for an Ivy League education
 
                             Bilal Ahmed
                             Cornell University
@@ -170,11 +170,11 @@ const Slider2 = () => {
                             <Star size={20} className='text-yellow-400 ' />
                         </div>
                         <p className='line-clamp-4 text-lg  text-gray-400'>
-                            The Common App felt like a labyrinth, and applying to  Yale seemed like a distant dream. Edify Elite became my guiding light! Their team,   particularly their understanding of Pakistani cultural nuances, helped me confidently navigate the application. They didn't just focus on forms; they helped me craft a compelling narrative that resonated with admissions officers. I'm convinced Edify Elite played a major role in my acceptance to Yale!
+                            The Common App felt like a labyrinth and applying to  Yale seemed like a distant dream Edify Elite became my guiding light Their team   particularly their understanding of Pakistani cultural nuances helped me confidently navigate the application They didnt just focus on forms they helped me craft a compelling narrative that resonated with admissions officers Im convinced Edify Elite played a major role in my acceptance to Yale
 
                             Amina Khan
                             Yale University
-                            Ivy League dreams were always on my mind, but standing out in the competitive pool felt impossible. Partnering with Edify Elite was a game-changer. Their expert team provided valuable insights on impressing admissions committees at top universities. They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively. Thanks to Edify Elite, I'm celebrating my acceptance to Cornell University! I highly recommend them to any student aiming for an Ivy League education.
+                            Ivy League dreams were always on my mind but standing out in the competitive pool felt impossible Partnering with Edify Elite was a game changer Their expert team provided valuable insights on impressing admissions committees at top universities They helped me refine my essays and activities section to highlight my strengths and achievements while showcasing my Pakistani background positively Thanks to Edify Elite Im celebrating my acceptance to Cornell University I highly recommend them to any student aiming for an Ivy League education
 
                             Bilal Ahmed
                             Cornell University
@@ -192,6 +192,9 @@ const Slider2 = () => {
 
                 </div>
             </SwiperSlide>
+
+
+
 
 
         </Swiper>
